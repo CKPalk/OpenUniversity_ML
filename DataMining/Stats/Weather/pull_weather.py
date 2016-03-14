70ef2458a9eb500c8877ea27c4e3e70ab5192682
Cameron Palk <ckpalk@comcast.net>
2016-03-14 12:31:13 -0700

    more weather

--- DataMining/Stats/Weather/pull_weather.py
+++ DataMining/Stats/Weather/pull_weather.py
@@ -10,59 +10,85 @@ key = '7ad6e966485527aa'
 
 def getWeatherJSON( day, month, year ):
 
-    url = "http://api.wunderground.com/api/{key}/history_{YYYY}{MM:0>2d}{DD:0>2d}/q/WA/Seattle.json".format(
-        key     = key,
-        YYYY    = year,
-        MM      = month,
-        DD      = day )
+	url = "http://api.wunderground.com/api/{key}/history_{YYYY}{MM:0>2d}{DD:0>2d}/q/OR/Eugene.json".format(
+		key	 = key,
+		YYYY = year,
+		MM	 = month,
+		DD	 = day )
 
-    request = Request( url )
-    with urlopen( request ) as response:
-        response = response.read()
-    parsed_json = json.loads( response.decode('utf-8') )
-    return parsed_json
-
-
-def appendTempIfNeeded( df ):
-    if 'Temp' not in df.columns:
-        df['Temp'] = np.nan
-    return df
+	request = Request( url )
+	with urlopen( request ) as response:
+		response = response.read()
+	parsed_json = json.loads( response.decode('utf-8') )
+	return parsed_json
 
 
-def findBestTemp( date ):
-    json = getWeatherJSON( date.day, date.month, date.year )
-    
+def appendAttrsIfNeeded( df ):
+	if 'Temp' not in df.columns:
+		df['Temp'] = np.nan
+	if 'Rain' not in df.columns:
+		df['Rain'] = np.nan
+	return df
+
+
+def findTempAndRain( date ):
+	json = getWeatherJSON( date.day, date.month, date.year )
+
+	observation = json['history']['observations'][date.hour - 1]
+
+	temperature = float( observation[ 'tempi' ] )
+	if temperature < -50:
+		temperature = None
+	
+	rain = int( observation[ 'rain' ] )
+
+	return ( temperature, rain )
+
+	
+
+
+
+	temperature = json['history']['observations'][date.hour - 1]['tempi']
+
+	
 
 
 
 def addWeatherData( csv_filename ):
 
-    df = pd.read_csv( csv_filename, parse_dates=['Dates'], date_parser=lambda
-            x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S' ) )
+	df = pd.read_csv( csv_filename, parse_dates=['Dates'], date_parser=lambda
+			x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S' ) )
 
-    df = appendTempIfNeeded( df )
+	df = appendAttrsIfNeeded( df )
 
-    for idx, row in df.iterrows():
-        if pd.isnull( row['Temp'] ):
-            print( "Found a null temperature, looking up temp for", row['Dates'] )
-            row_temp = findBestTemp( row['Dates'] )
+	for idx, row in df.iterrows():
+		if pd.isnull( row['Temp'] ) and pd.isnull( row['Rain'] ):
+			temp_rain = findTempAndRain( row['Dates'] )
+			print( "Setting temperature and rain for row", idx )
+			df.set_value( idx, 'Temp', temp_rain[0] )
+			df.set_value( idx, 'Rain', int( temp_rain[1] ) )
+			break
+		else:
+			print( "Found temperature", row['Temp'] )
+			print( "Found rain", row['Rain'] )
+			print()
 
-            return
-        else:
-            print( "Found temperature", row['Temp'] )
+	df.to_csv( csv_filename, index=False )
+
+	print( "File saved" )
 
 
 
 
 
 
-    #response = getWeatherJSON( 9, 6, 1994 )
-    #print( response['history']['dailysummary'][0]['meantempi'] )
+	#response = getWeatherJSON( 9, 6, 1994 )
+	#print( response['history']['dailysummary'][0]['meantempi'] )
 
 def main( argv ):
-    addWeatherData( argv[1] )
+	addWeatherData( argv[1] )
 
 
 if __name__ == '__main__':
-    main( sys.argv )
+	main( sys.argv )
 
